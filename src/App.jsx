dfef45fb5b9cd39a9f8ce2d54e3bb3a49f2c6fae
Sumanth_Kumar <41052225+sumanthkumar1997@users.noreply.github.com>
2025-08-29 14:31:18 +0530

    Cell created.

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,40 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const config = [ 
+    [1,1,1],
+    [1,0,1],
+    [1,1,1]
+  ];
+
+  const [activeCells, setActiveCells] = useState([]);
+
+  const Cell = ({key}) => {
+
+    return(<div
+      key={key}
+      className={`cell ${activeCells.includes(key) ? 'cell-active' : ''}`}
+      onClick={()=>{setActiveCells([...activeCells, key])}}>
+
+    </div>);
+
+  }
 
   return (
     <>
+    <div className='grid'>
+      {config.flat(1).map((item, index)=>(
+         (item === 1) ? 
+        (<Cell key={index} />)
+        : (<span/>) 
+      ))}
+
+    </div>
       
-     
     </>
   )
 }
+
+
 
 export default App
